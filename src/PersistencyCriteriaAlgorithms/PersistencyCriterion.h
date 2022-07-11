2bc62b10ba44e76ca0046cac73039b6d6bd29f13
david <david.nicklaser@mailbox.tu-dresden.de>
2022-07-11 16:34:45 +0200

    make function generic

--- src/PersistencyCriteriaAlgorithms/PersistencyCriterion.h
+++ src/PersistencyCriteriaAlgorithms/PersistencyCriterion.h
@@ -18,6 +18,31 @@ public:
     PersistencyCriterion(Instance& instance, Constraints& constraints, int n_vertices)
         : instance{instance}, constraints{constraints}, n_vertices{n_vertices}{
     }
+
+
+
+    template<typename T>
+    T evaluate_triples_to_one_fixed_vertex(int u, T (*evaluate_triple)(T, int, int, int, operator)){
+        float sum;
+        for(int i{0}; i<n_vertices; i++){ if(i==u) continue;
+            for(int j{i+1}; j<n_vertices; j++){ if(j==u) continue;
+                sum = sum operator evaluate_triple(u,i,j);
+            }
+        }
+        return sum;
+    }
+
+    template<typename T>
+    T evaluate_triples_to_two_fixed_vertices(int u, int v, T (*evaluate_triple)(T, int, int, int, operator)){
+        float sum{0};
+        for(int i{0}; i<n_vertices; i++){ if(i==u || i==v) continue;
+            sum = sum operator evaluate_triple(u,v,i);
+        }
+        return sum;
+    }
+
+
+
 };
 
 #endif //PARTIALLY_OPTIMAL_SUBSPACE_CLUSTERING_PERSISTENCYCRITERION_H
--- src/PersistencyCriteriaAlgorithms/TriangleCutCriterion.h
+++ src/PersistencyCriteriaAlgorithms/TriangleCutCriterion.h
@@ -1,4 +1,31 @@
 #ifndef PARTIALLY_OPTIMAL_SUBSPACE_CLUSTERING_TRIANGLECUTCRITERION_H
 #define PARTIALLY_OPTIMAL_SUBSPACE_CLUSTERING_TRIANGLECUTCRITERION_H
 
+
+#include "PersistencyCriterion.h"
+
+class TriangleCutCriterion: public PersistencyCriterion {
+
+
+public:
+    TriangleCutCriterion(Instance &instance, Constraints &constraints, int n_vertices)
+            : PersistencyCriterion{instance, constraints, n_vertices} {
+    }
+
+    std::vector<float> create_costs_neg_singlet(){
+        std::vector<float> singlets(n_vertices*(((n_vertices-1)*(n_vertices-2))/2));
+        for(int u{0}; u<n_vertices; u++){
+            float singlet_cost;
+            for(int i{0}; i<n_vertices; i++){ if(i==u) continue;
+                for(int j{i+1}; j<n_vertices; j++){ if(j==u) continue;
+                    singlet_cost += std::abs(instance.get_cost(u, i, j));
+                }
+            }
+            singlets.push_back(singlet_cost);
+        }
+        return singlets;
+    }
+};
+
+
 #endif //PARTIALLY_OPTIMAL_SUBSPACE_CLUSTERING_TRIANGLECUTCRITERION_H
